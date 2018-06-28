38348f9239e5fa6a7ae7172ba00c523b561e5dfe
dai <dainv@blueseed.tv>
2018-06-28 18:34:48 +0700

    11

--- resources/assets/js/admin/components/Aside.vue
+++ resources/assets/js/admin/components/Aside.vue
@@ -2,7 +2,7 @@
   <aside class="aside-menu">
     <div class="aside-avatar">
       <div class="aside-avatar-border"><img src="http://s2.chacha.vn/albums/s1/1403/1403.jpg" alt="avatar"></div>
-      <h4>Bảo Thy</h4>
+      <h4><i>Ms. </i>Bảo Thy</h4>
     </div>
     <div class="aside-info">
 
