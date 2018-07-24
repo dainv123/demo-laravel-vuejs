f9469664ac4dcc17981d20dc96bdfa8714d31300
dai nguyen <ngdai0402@gmail.com>
2018-07-24 23:13:28 +0700

    push

--- public/js/custom.js
+++ public/js/custom.js
@@ -2,3 +2,9 @@
 // var next = document.querySelectorAll('a.footer__navigation__page-btn.disabled')[1].firstChild;
 // prev.innerHTML = '<i class="fa fa-chevron-left" />';
 // next.innerHTML = '<i class="fa fa-chevron-right" />';
+var tbtn_toggle_menu = document.querySelector('.sidebar-minimizer');
+var toggle_menu = document.querySelector('.app-header .sidebar-toggler');
+tbtn_toggle_menu.onclick = function () {
+    toggle_menu.classList.toggle('visibility-hidden');
+}
+
